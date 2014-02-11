1cb4a555a47a649fa8d328a6864cf55ac8df8329
wencheng <wencheng.f2e@gmail.com>
2014-02-11 10:38:36 +0800

    add js-ui category

---
+++ app/js/data/js-ui.js
@@ -0,0 +1,12 @@
+define({
+	name: 'js-ui插件',
+	description: 'ui相关的js插件',
+	items: [
+		{
+			name: '绚丽加载效果',
+			description: '特别牛的加载效果，不太适合大众网站',
+			url: 'http://tympanus.net/Development/CreativeLoadingEffects/'
+		}
+	],
+	items_count: 
+});
